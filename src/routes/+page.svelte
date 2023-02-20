b58ba6fe503f63d423d452654ed2112d2f0eee79
Steven Bell <dev.thebells1111@gmail.com>
2023-02-20 13:59:20 -0600

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,145 @@
+<script>
+	import { parse } from 'fast-xml-parser';
+	import { decode } from 'html-entities';
+	import parseSRT from 'parse-srt';
+	import VirtualList from 'svelte-tiny-virtual-list';
+	import Transcripts from './Transcripts.svelte';
+
+	const parserOptions = {
+		attributeNamePrefix: '@_',
+		//attrNodeName: false,
+		//textNodeName : "#text",
+		ignoreAttributes: false,
+		ignoreNameSpace: false,
+		attrValueProcessor: (val, attrName) => decode(val), //default is a=>a
+		tagValueProcessor: (val, tagName) => decode(val) //default is a=>a
+	};
+
+	let feed = {};
+	let selectedEpisode = {};
+	let item = [];
+	let searchResults = [];
+	let searchQuery;
+	let searchInput = '';
+
+	let feedUrl = '';
+	// feedUrl = 'https://feeds.noagendaassets.com/noagenda.xml'
+	// searchInput = 'curio caster';
+
+	function searchTranscripts() {
+		searchResults = [];
+		selectedEpisode = {};
+		searchQuery = searchInput;
+		for (const [i, v] of feed.item.entries()) {
+			let results = [...v?.fetchedTranscript.matchAll(new RegExp(searchQuery, 'gi'))].map(
+				(a) => a.index
+			);
+			if (results.length) {
+				searchResults.push([i, results]);
+			}
+		}
+	}
+
+	async function fetchTranscript() {
+		let res = await fetch(feedUrl);
+		let data = await res.text();
+		let xml2Json = parse(data, parserOptions);
+
+		let xmlJson = xml2Json;
+
+		feed = xmlJson.rss.channel;
+		let _item = [].concat(feed.item);
+		for (const [i, v] of _item.entries()) {
+			if (v['podcast:transcript']) {
+				let res = await fetch(v['podcast:transcript']['@_url']);
+				let data = await res.text();
+				_item[i].fetchedTranscript = data;
+			}
+		}
+
+		feed.item = _item;
+	}
+</script>
+
+<main>
+	<fetch-feed>
+		<input bind:value={feedUrl} />
+		<button on:click={fetchTranscript}>Get Feed</button>
+	</fetch-feed>
+
+	<h1>{feed?.title || ''}</h1>
+	{#if feed?.title}
+		<search-transcripts>
+			<input bind:value={searchInput} />
+			<button on:click={searchTranscripts}>Search Transcripts</button>
+		</search-transcripts>
+		{#if searchResults?.length}
+			<pane-container>
+				<left-pane>
+					<h3>Episodes</h3>
+					<ul>
+						{#each searchResults || [] as result}
+							<li
+								on:click={() => {
+									selectedEpisode = feed.item[result[0]];
+								}}
+							>
+								{feed.item[result[0]].title} - {result[1].length} occurrence{`${
+									result[1].length > 1 ? 's' : ''
+								}`}
+							</li>
+						{/each}
+					</ul>
+				</left-pane>
+				<right-pane>
+					<Transcripts episode={selectedEpisode} {searchQuery} />
+				</right-pane>
+			</pane-container>
+		{/if}
+	{/if}
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+	}
+
+	fetch-feed,
+	search-transcripts {
+		width: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	h1 {
+		text-align: center;
+	}
+
+	input {
+		width: 50%;
+	}
+	pane-container {
+		display: flex;
+		justify-content: space-between;
+		width: 100%;
+		height: calc(100vh - 138px);
+		overflow: hidden;
+	}
+
+	li {
+		list-style: none;
+		padding: 8px 0;
+		color: blue;
+		text-decoration: underline;
+		cursor: pointer;
+	}
+	left-pane,
+	right-pane {
+		width: 50%;
+		overflow: auto;
+		height: 100%;
+	}
+</style>
---
+++ src/routes/Transcripts.svelte
@@ -0,0 +1,235 @@
+<script>
+	import parseSRT from 'parse-srt';
+	import VirtualList from 'svelte-tiny-virtual-list';
+
+	export let episode;
+	export let searchQuery = '';
+
+	let transcriptIndex = 0;
+	let scrollToIndex = 0;
+	let currentIndex = 0;
+
+	let player;
+	let playerTime;
+
+	let episodeTranscript;
+
+	let listHeight = 0;
+	let scrollToAlignment = 'start';
+
+	let scrollStatus = 'Disabled';
+
+	let filteredIndices = [];
+
+	$: console.log(episode);
+
+	$: if (episode && episode.fetchedTranscript) {
+		console.log(episode['podcast:transcript']);
+		const transcriptSRT = episode?.['podcast:transcript']?.['@_type'] === 'application/srt';
+
+		if (transcriptSRT) {
+			let transcript = parseSRT(episode.fetchedTranscript);
+			let t = transcript
+				.map((v) => v.text)
+				.join(' ')
+				.replace(/(<|&lt;)br\s*\/*(>|&gt;)/g, ' ');
+
+			episodeTranscript = [...transcript];
+			transcript.full = t.split('|-|').join(' ');
+			searchTranscript(searchQuery);
+		} else {
+			episodeTranscript = null;
+		}
+	} else {
+		episodeTranscript = null;
+	}
+
+	function jumpToSection(section, index) {
+		if (section.start) {
+			$playerTime = section.start;
+			$player.currentTime = $playerTime;
+			transcriptIndex = index;
+		}
+	}
+
+	$: if (transcriptIndex && scrollStatus === 'Enabled') {
+		scrollToIndex = transcriptIndex;
+	}
+
+	function searchTranscript(transcriptSearchQuery) {
+		if (transcriptSearchQuery) {
+			scrollStatus = 'Disabled';
+			currentIndex = 0;
+			filteredIndices = getAllIndexes(episodeTranscript, transcriptSearchQuery);
+			if (filteredIndices.length > 0) {
+				transcriptIndex = filteredIndices[0];
+				setTimeout(() => {
+					scrollToIndex = transcriptIndex;
+				}, 10);
+
+				scrollToAlignment = 'center';
+			}
+		} else {
+			currentIndex = 0;
+			scrollToIndex = transcriptIndex || 0;
+			filteredIndices = [];
+			scrollToAlignment = 'start';
+		}
+
+		function getAllIndexes(arr, val) {
+			var indexes = [],
+				i;
+			for (i = 0; i < arr.length; i++)
+				if (arr[i].text.toLowerCase().includes(val.toLowerCase())) {
+					indexes.push(i);
+				}
+			return indexes;
+		}
+	}
+
+	function handleScrollStatus() {
+		scrollStatus = scrollStatus === 'Enabled' ? 'Disabled' : 'Enabled';
+	}
+
+	function getNextIndex() {
+		currentIndex++;
+		if (currentIndex === filteredIndices.length) {
+			currentIndex = 0;
+		}
+		scrollToIndex = undefined;
+		setTimeout(() => {
+			(scrollToIndex = filteredIndices[currentIndex]), 1;
+		});
+	}
+	function getPreviousIndex() {
+		currentIndex--;
+		if (currentIndex === -1) {
+			currentIndex = filteredIndices.length - 1;
+		}
+		scrollToIndex = undefined;
+		setTimeout(() => {
+			(scrollToIndex = filteredIndices[currentIndex]), 1;
+		});
+	}
+
+	function convertTime(time) {
+		try {
+			let formatTime = new Date(time * 1000).toISOString().substr(11, 8);
+			if (formatTime.charAt(0) === '0') {
+				return formatTime.substring(1);
+			}
+
+			return formatTime;
+		} catch {
+			(err) => console.log(err);
+		}
+	}
+</script>
+
+<div class="bar-2">
+	<button class="scrolling" on:click={handleScrollStatus}>
+		Scrolling {scrollStatus}
+	</button>
+	{#if filteredIndices.length > 0}
+		<div class="index-select">
+			<button class="previous" on:click={getPreviousIndex}>&#9664</button>
+			<span>
+				{currentIndex + 1} of {filteredIndices.length}
+			</span>
+			<button class="next" on:click={getNextIndex}>&#9654</button>
+		</div>
+	{/if}
+</div>
+
+{#if episodeTranscript?.length}
+	<div class="list-height" bind:clientHeight={listHeight}>
+		<VirtualList
+			height={listHeight}
+			width="100%"
+			itemCount={episodeTranscript.length}
+			itemSize={50}
+			overscanCount={5}
+			{scrollToIndex}
+			{scrollToAlignment}
+		>
+			<div
+				slot="item"
+				let:index
+				let:style
+				{style}
+				class="row"
+				class:active={index === transcriptIndex || index === filteredIndices[currentIndex]}
+				on:click={jumpToSection.bind(this, episodeTranscript?.[index], index)}
+			>
+				<p class="transcript-text">
+					{@html episodeTranscript?.[index].text || ''}
+				</p>
+				<p class="transcript-time">
+					{convertTime(episodeTranscript?.[index].start) ?? ''}
+				</p>
+			</div>
+		</VirtualList>
+	</div>
+{/if}
+
+<style>
+	.list-height {
+		height: calc(100% - 52px);
+	}
+
+	div {
+		display: flex;
+	}
+
+	div.active p {
+		font-weight: 700;
+		color: var(--accent-color-blue);
+	}
+
+	.transcript-text {
+		flex: 1 1 auto;
+	}
+
+	.transcript-time {
+		padding-right: 8px;
+	}
+
+	.bar-2 {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		padding: 8px;
+		border-bottom: 1px solid var(--border-color);
+	}
+
+	.index-select > span {
+		width: 100px;
+		text-align: center;
+		display: inline-flex;
+		align-items: center;
+		justify-content: center;
+		color: var(--primary-text-color);
+	}
+
+	button {
+		background-color: transparent;
+		border: none;
+		padding: 0;
+		margin: 0;
+		color: var(--primary-text-color);
+		height: 36px;
+		width: 36px;
+		text-align: center;
+		cursor: pointer;
+	}
+	.scrolling {
+		width: auto;
+	}
+
+	.next {
+		text-align: start;
+	}
+	.previous {
+		text-align: end;
+	}
+</style>
